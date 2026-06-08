89e57961d32795cdb3c1d6ba96273951790c7296
Julian Ramirez Reyes <julianramirezreyes23@gmail.com>
2026-06-07 20:00:55 -0500

    build: update Flutter build artifacts and generated web assets

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1914612742" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "898024020" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
